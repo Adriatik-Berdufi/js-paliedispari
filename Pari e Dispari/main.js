e9e75152e62362095e23970f6d87beada830298a
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-01-18 08:08:23 +0100

    feat: add function somma

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -7,7 +7,7 @@ const result = document.getElementById("result");
 
 
 
-let risultato = randomNr(1, 9);
+let risultato = somma(1, 9);
 console.log(risultato);
 
 
@@ -20,3 +20,9 @@ function randomNr(min, max){
     let nr = Math.floor(Math.random() * (max - min) + min) ;
     return (nr);
 };
+
+//** somma tra 2 nr 
+function somma(n1, n2){
+    let sommaNumeri = n1 + n2;
+    return(sommaNumeri);
+}
